c096062565f56e4fa8586bfdbb651c43e90fe4e0
Ben Barnard <barnardb@gmail.com>
2017-04-26 19:24:57 +0200

    Remove JsonParser.parseRepeatedly
    This method has been superseded by FramedStream, and should have been
    removed earlier.

--- sdk/src/main/java/com/hashicorp/nomad/javasdk/JsonParser.java
+++ sdk/src/main/java/com/hashicorp/nomad/javasdk/JsonParser.java
@@ -1,11 +1,8 @@
 package com.hashicorp.nomad.javasdk;
 
-import com.fasterxml.jackson.core.JsonLocation;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JavaType;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
 
 /**
  * Wraps a Jackson ObjectMapper to parse values of a specific type.
@@ -31,37 +28,5 @@ class JsonParser<T> implements ValueExtractor<T> {
                     json,
                     e);
         }
-    }
-
-    /**
-     * Parses as many values as possible.
-     *
-     * @param buffer   the data to parse
-     * @param callback invoked once for each value parsed
-     * @return the buffer containing any unparsed bytes, or {@code null} if the data was fully consumed.
-     * @throws ResponseParsingException if an error occurs during parsing
-     */
-    public ByteBuffer parseRepeatedly(ByteBuffer buffer, Callback<T> callback) throws NomadException {
-        JsonLocation checkpoint = null;
-        try {
-            com.fasterxml.jackson.core.JsonParser jacksonParser = NomadJson.OBJECT_MAPPER.getFactory()
-                    .createParser(buffer.array(), buffer.position(), buffer.remaining());
-            while (jacksonParser.nextToken() != null) {
-                callback.apply(NomadJson.OBJECT_MAPPER.<T>readValue(jacksonParser, responseEntityType));
-                checkpoint = jacksonParser.getTokenLocation();
-            }
-        } catch (IOException e) {
-            if (e instanceof JsonProcessingException && e.getMessage().startsWith("Unexpected end-of-input")) {
-                if (checkpoint != null)
-                    buffer.position((int) checkpoint.getByteOffset());
-                return buffer;
-            } else {
-                throw new ResponseParsingException(
-                        "Unable to parse next StreamFrame from response body JSON: " + e,
-                        null,
-                        e);
-            }
-        }
-        return null;
     }
 }
