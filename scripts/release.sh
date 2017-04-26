d34f0e0bd87d6e2538264a787e81f91dd0df8a5a
Ben Barnard <barnardb@gmail.com>
2017-04-26 18:11:39 +0200

    Add (untested) release script

---
+++ scripts/release.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+[ $# -eq 2 ] || {
+  echo "usage: $0 <release-version> <next-working-version>"
+  echo
+  echo 'E.g: $0 0.0.1 0.0.2-SNAPSHOT'
+  exit 2
+} &>2
+release_version="$1"
+next_working_version="$2"
+
+[ -z "$(git status --porcelain)" ] || {
+  echo "Your working directoy is not pristine"
+  echo
+  git status
+  exit 1
+} &>2
+
+mvn versions:set -DgenerateBackupPoms=false -DnewVersion="${release_version}"
+git add -u
+mvn deploy
+git commit -m "Setting version to ${release_version}"
+[ -z "$(git status --porcelain)" ] || {
+  echo "Your working directory is not pristine after deploying"
+  echo
+  git status
+  exit 1
+} &>2
+git tag "v${release_version}" -m "Releasing ${release_version}"
+
+mvn versions:set -DgenerateBackupPoms=false -DnewVersion="${next_working_version}"
+git add -u
+git commit -m "Setting version to ${next_working_version}"
