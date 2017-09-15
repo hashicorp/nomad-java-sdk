552b5ccec7a634c738fd1893ba75997207850af6
Ben Barnard <barnardb@gmail.com>
2017-09-15 10:06:48 +0200

    Add a script for regenerating the API model JavaBeans

---
+++ scripts/generate.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+# Generates the API model JavaBeans using reflection on the golang nomad/api package
+
+# move to the root directory of the repository
+cd "$(dirname "$0")/.."
+
+go run generator/*.go sdk/src/main/java/com/hashicorp/nomad/apimodel/
