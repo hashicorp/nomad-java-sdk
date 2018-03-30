653acdcd89dfcefd57451d7eb553bd9e14cdc34f
Ben Barnard <barnardb@gmail.com>
2018-03-30 17:50:48 +0200

    Stop test processes when the the JVM exits

--- testkit/src/main/java/com/hashicorp/nomad/testutils/AugmentedProcess.java
+++ testkit/src/main/java/com/hashicorp/nomad/testutils/AugmentedProcess.java
@@ -7,10 +7,12 @@ import java.util.List;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 /**
- * Encapsulates a {@link java.lang.Process} with threads that pump its stdout and stdin to {@link PrintWriter}s.
+ * Encapsulates a {@link java.lang.Process} with threads that pump its stdout and stdin to {@link PrintWriter}s,
+ * and destroys the process when the JVM exits.
  */
 class AugmentedProcess {
     private final Process process;
+    private final Thread shutdownHook;
     private final LinePump stdoutPump;
     private final LinePump stderrPump;
 
@@ -19,15 +21,21 @@ class AugmentedProcess {
     }
 
     AugmentedProcess(List<String> command, PrintWriter stdoutSink, PrintWriter stderrSink) throws IOException {
-        process = Runtime.getRuntime().exec(command.toArray(new String[] {}));
+        process = Runtime.getRuntime().exec(command.toArray(new String[]{}));
         try {
+            shutdownHook = new Thread() {
+                @Override
+                public void run() {
+                    process.destroy();
+                }
+            };
+            Runtime.getRuntime().addShutdownHook(shutdownHook);
             stdoutPump = new LinePump("stdout", process.getInputStream(), UTF_8, stdoutSink);
             stderrPump = new LinePump("stderr", process.getErrorStream(), UTF_8, stderrSink);
             stdoutPump.start();
             stderrPump.start();
         } catch (Exception e) {
-            process.destroy();
-            waitUntilStopped();
+            close();
             throw e;
         }
     }
@@ -42,6 +50,7 @@ class AugmentedProcess {
             while (true) {
                 try {
                     process.waitFor();
+                    Runtime.getRuntime().removeShutdownHook(shutdownHook);
                     return;
                 } catch (InterruptedException e) {
                     // we'll try agin
